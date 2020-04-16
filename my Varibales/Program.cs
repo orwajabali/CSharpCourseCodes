270e895d1ae1e50626f702e15bdcf98eaec35cca
orwa jabali <63741250+orwajabali@users.noreply.github.com>
2020-04-16 22:59:24 +0300

    First variable
    This my first variable declartion

---
+++ my Varibales/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_Varibales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =7;
+            Console.WriteLine(" My virabale value = {0}", x);
+            Console.ReadKey();
+        }
+    }
+}
